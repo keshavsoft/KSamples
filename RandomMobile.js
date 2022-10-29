242746dbd0b8a3da0ec04613ead967d1fc39934d
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2022-10-29 09:16:35 +0530

    Create RandomMobile.js

---
+++ RandomMobile.js
@@ -0,0 +1,7 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+};
+
+getRandomInt(6000000001, 9999999999)
